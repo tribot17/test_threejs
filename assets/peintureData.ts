db5677164931e3913fe6c4f7bbefd636b0876815
tribot17 <tribot17@gmail.com>
2023-08-18 17:39:18 +0200

    update texture

--- assets/peintureData.ts
+++ assets/peintureData.ts
@@ -1,13 +1,28 @@
 import { peintureInterface } from "@/interface/peintureInterface";
-import painting1 from "@/assets/radeau_meduse.jpeg";
-import painting2 from "@/assets/potw2138a.jpg";
-import painting3 from "@/assets/wall.jpg";
-import painting4 from "@/assets/wood_texture.jpg";
+import painting1 from "@/assets/radeauDeLaMeduse.jpeg";
+import painting2 from "@/assets/peinting2.jpg";
+import painting3 from "@/assets/mortDeSocrate.jpeg";
+import painting4 from "@/assets/Le_Désespere.jpg";
 
 export const peitureData: peintureInterface[] = [
   {
     position: [0, 5, -24.5],
     rotation: [0, 0, 0],
+    img: painting2.src,
+    text: {
+      date: "1489-1490",
+      titre: "  ",
+      peintre: "Sandro Botticelli",
+      description: `L'Annonciation dite du Cestello (en italien Annunciazione di Cestello) est une peinture de Sandro Botticelli réalisée en 1489 et 1490 en tempera sur un panneau de 150
+      × 156 cm, restaurée en 1978 et conservée au musée des Offices de
+      Florence, œuvre caractéristique par son pavement perspectif,
+       son paysage en fond, les poses très étudiées des protagonistes.`,
+    },
+    link: "https://fr.wikipedia.org/wiki/L%27Annonciation_du_Cestello",
+  },
+  {
+    position: [0, 5, 24.5],
+    rotation: [0, -Math.PI, 0],
     img: painting1.src,
     text: {
       date: "1818 - 1819",
@@ -21,31 +36,24 @@ export const peitureData: peintureInterface[] = [
        représente un épisode tragique de l'histoire de la marine coloniale française : 
        le naufrage de la frégate Méduse.`,
     },
-    link: "",
-  },
-  {
-    position: [0, 5, 24.5],
-    rotation: [0, -Math.PI, 0],
-    img: painting2.src,
-    text: {
-      date: "1585",
-      titre: "",
-      peintre: "c moi",
-      description: "Cette peinture est jolie",
-    },
-    link: "",
+    link: "https://fr.wikipedia.org/wiki/Le_Radeau_de_La_M%C3%A9duse",
   },
   {
     position: [24.5, 5, 0],
     rotation: [0, Math.PI / 2, 0],
     img: painting3.src,
     text: {
-      date: "1585",
-      titre: "",
-      peintre: "c moi",
-      description: "Cette peinture est jolie",
+      date: "1843-1845",
+      titre: "La Mort de Socrate",
+      peintre: "Jacques-Louis David",
+      description: `La Mort de Socrate est un tableau réalisé par le peintre français Jacques-Louis David en 1787. 
+      Il représente la mort du philosophe grec Socrate, condamné par les Athéniens à boire la ciguë.
+      Le tableau avait été commandé à David par Charles-Michel Trudaine de la Sablière. 
+      Il a été présenté la première fois au Salon de peinture et de sculpture de l'année 1787,
+      en compagnie d'une autre peinture sur le même sujet de Pierre Peyron. Il est actuellement 
+      conservé au Metropolitan Museum of Art, à New York1.`,
     },
-    link: "",
+    link: "https://fr.wikipedia.org/wiki/Le_D%C3%A9sesp%C3%A9r%C3%A9",
   },
 
   {
@@ -53,11 +61,16 @@ export const peitureData: peintureInterface[] = [
     rotation: [0, Math.PI / 2, 0],
     img: painting4.src,
     text: {
-      date: "1585",
-      titre: "",
-      peintre: "c moi",
-      description: "Cette peinture est jolie",
+      date: "1843-1845",
+      titre: "Le Désespéré",
+      peintre: "Gustave Courbet",
+      description: `L'œuvre est un autoportrait de Gustave Courbet qui se représente 
+      de face, en gros plan, la bouche entrouverte et le regard plongeant dans celui 
+      du spectateur. Ses yeux sont écarquillés et ses deux mains semblent lui arracher 
+      les cheveux. La pâleur du visage contraste avec la noirceur des cheveux et de la barbe, 
+      le tout renforcé par la blancheur de la chemise.
+      La source lumineuse vient d'en haut, sur la gauche, et accentue les contrastes.`,
     },
-    link: "",
+    link: "https://fr.wikipedia.org/wiki/Le_D%C3%A9sesp%C3%A9r%C3%A9",
   },
 ];
