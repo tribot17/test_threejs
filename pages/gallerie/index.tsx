bbaf7946e34a4401f93d64ec8bb753bc6c03e86f
tribot17 <tribot17@gmail.com>
2023-08-18 19:14:11 +0200

    add a loader

--- pages/gallerie/index.tsx
+++ pages/gallerie/index.tsx
@@ -1,7 +1,7 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Physics } from "@react-three/cannon";
-import { SpotLight } from "@react-three/drei";
-import { useRef, useState } from "react";
+import { Loader, useProgress } from "@react-three/drei";
+import { useRef, useState, useEffect } from "react";
 import { peitureData } from "@/assets/peintureData";
 import * as THREE from "three";
 import Wall from "@/components/Wall";
@@ -10,6 +10,8 @@ import Painting from "@/components/Peinting";
 import UIContainer from "@/components/UiContainer";
 import Controls from "../../components/Controls";
 import Floor from "@/components/Plane";
+import LoadingScreen from "@/components/LoadingScreen";
+import { Spinner } from "@chakra-ui/react";
 
 //Opti avoir 1 seul useFrame dans le projet
 const Zoom: React.FC<any> = ({ cameraRef, zoom }) => {
@@ -30,72 +32,97 @@ const Zoom: React.FC<any> = ({ cameraRef, zoom }) => {
 
 const App = () => {
   const [zoom, setZoom] = useState(false);
+  const [load, setLoad] = useState(false);
   const cameraRef = useRef<THREE.PerspectiveCamera>();
-  //C
+  const { progress } = useProgress();
+
   return (
     <>
-      <UIContainer />
-      <Canvas
-        style={{ width: "100%", height: "100vh" }}
-        shadows
-        camera={{ fov: zoom ? 15 : 35 }}
-        dpr={[1, 2]}
-        onClick={() => setZoom(!zoom)}
+      <div
+        style={
+          progress === 100
+            ? { visibility: "hidden", height: "0" }
+            : {
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                visibility: "visible",
+                height: "100vh",
+                width: "100%",
+              }
+        }
       >
-        <ambientLight intensity={1} />
-        <fog attach={"fog"} args={["black", 60, 100]} />
-        <spotLight
-          args={["#ffffff", 1.5, 7, (Math.PI / 180) * 45, 0.4]}
-          position={[-3, 1, 0]}
-          castShadow
-        />
-        <Physics>
-          <Controls />
-          <Zoom cameraRef={cameraRef} zoom={zoom} />
-          <Floor />
-          <Wall rotation={[0, 0, 0]} position={[0, 0, -25]} color={"#565c73"} />
-          {peitureData &&
-            peitureData.map((peinture, i) => (
-              <group key={i}>
-                <spotLight
-                  intensity={150}
-                  color={"#e3d2ca"}
-                  position={new THREE.Vector3().fromArray(peinture.position)}
-                  angle={Math.PI}
-                />
+        <Spinner />
+      </div>
+      <div style={{ visibility: progress === 100 ? "visible" : "hidden" }}>
+        <UIContainer />
 
-                <Painting
-                  position={peinture.position}
-                  rotation={peinture.rotation}
-                  img={peinture.img}
-                  text={peinture.text}
-                  link={peinture.link}
-                  index={i}
-                />
-              </group>
-            ))}
-          <Wall
-            rotation={[-Math.PI, 0, 0]}
-            position={[0, 0, 25]}
-            color={"#565c73"}
+        <Canvas
+          style={{ width: "100%", height: "100vh" }}
+          shadows
+          camera={{ fov: zoom ? 15 : 35 }}
+          dpr={[1, 2]}
+          onClick={() => setZoom(!zoom)}
+        >
+          <ambientLight intensity={1} />
+          <fog attach={"fog"} args={["black", 60, 100]} />
+          <spotLight
+            args={["#ffffff", 1.5, 7, (Math.PI / 180) * 45, 0.4]}
+            position={[-3, 1, 0]}
+            castShadow
           />
-          <Wall
-            rotation={[0, Math.PI / 2, 0]}
-            position={[-25, 0, 0]}
-            color={"#565c73"}
-          />
-          <Wall
-            rotation={[0, -Math.PI / 2, 0]}
-            position={[25, 0, 0]}
-            color={"#565c73"}
-          />
-          <Wall
-            rotation={[Math.PI / 2, 0, 0]}
-            position={[0, 25, 0]}
-            color={"#565c73"}
-          />
-        </Physics>
-      </Canvas>
+          <Physics>
+            <Controls />
+            <Zoom cameraRef={cameraRef} zoom={zoom} />
+            <Floor />
+            <Wall
+              rotation={[0, 0, 0]}
+              position={[0, 0, -25]}
+              color={"#565c73"}
+            />
+            {peitureData &&
+              peitureData.map((peinture, i) => (
+                <group key={i}>
+                  <spotLight
+                    intensity={150}
+                    color={"#e3d2ca"}
+                    position={new THREE.Vector3().fromArray(peinture.position)}
+                    angle={Math.PI}
+                  />
+
+                  <Painting
+                    position={peinture.position}
+                    rotation={peinture.rotation}
+                    img={peinture.img}
+                    text={peinture.text}
+                    link={peinture.link}
+                    index={i}
+                  />
+                </group>
+              ))}
+            <Wall
+              rotation={[-Math.PI, 0, 0]}
+              position={[0, 0, 25]}
+              color={"#565c73"}
+            />
+            <Wall
+              rotation={[0, Math.PI / 2, 0]}
+              position={[-25, 0, 0]}
+              color={"#565c73"}
+            />
+            <Wall
+              rotation={[0, -Math.PI / 2, 0]}
+              position={[25, 0, 0]}
+              color={"#565c73"}
+            />
+            <Wall
+              rotation={[Math.PI / 2, 0, 0]}
+              position={[0, 25, 0]}
+              color={"#565c73"}
+            />
+          </Physics>
+        </Canvas>
+      </div>
     </>
   );
 };
